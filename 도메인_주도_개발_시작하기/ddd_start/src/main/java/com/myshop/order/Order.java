723bf843cb157e9849303b9b91d6fc60078a9c1b
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 12:20:41 +0900

    write READE.md - add Requirement

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/Order.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/Order.java
@@ -9,6 +9,10 @@ public class Order {
 		this.shippingInfo = shippingInfo;
 	}
 
+	public void changeShipped(){
+		// todo: implement logic
+	}
+
 	public void changeShippingInfo(ShippingInfo newShippingInfo) {
 		if (!isShippingChangeable()) {
 			throw new IllegalStateException("can't change shipping in " + state);
@@ -19,5 +23,13 @@ public class Order {
 	private boolean isShippingChangeable() {
 		return state == OrderState.PAYMENT_WAITING ||
 			state == OrderState.PREPARING;
+	}
+
+	public void cancel(){
+		// todo: implement logic
+	}
+
+	public void completePayment(){
+		// todo: implement logic
 	}
 }
