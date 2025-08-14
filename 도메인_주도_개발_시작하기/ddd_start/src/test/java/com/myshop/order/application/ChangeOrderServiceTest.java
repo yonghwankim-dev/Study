8a2952ad6b434a75e03b1c064844f845c71baaca
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 15:19:08 +0900

    imple changeOrderService

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/application/ChangeOrderServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/application/ChangeOrderServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 
+import com.myshop.member.domain.Member;
 import com.myshop.member.domain.MemberId;
 import com.myshop.member.domain.MemberRepository;
 import com.myshop.order.domain.Address;
@@ -30,6 +31,8 @@ class ChangeOrderServiceTest {
 	private OrderNo id;
 	private OrderRepository orderRepository;
 	private MemberRepository memberRepository;
+	private ChangeOrderService service;
+	private MemberId memberId;
 
 	private ShippingInfo createNewShippingInfo() {
 		Receiver newReceiver = new Receiver("Jane Doe", "0987654321");
@@ -40,35 +43,40 @@ class ChangeOrderServiceTest {
 	@BeforeEach
 	void setUp() {
 		id = new OrderNo("12345");
-		MemberId memberId = new MemberId("23456");
+		memberId = new MemberId("23456");
 		Orderer orderer = new Orderer(memberId, "John Doe", "johnDoe@gmail.com");
 		OrderLine orderLine = new OrderLine(new Product(), new Money(1000), 2);
 		List<OrderLine> orderLines = List.of(orderLine);
-		ShippingInfo shippingInfo = new ShippingInfo(
-			new Receiver("John Doe", "1234567890"),
-			new Address("123 Main St", "City", "12345")
-		);
+		Receiver receiver = new Receiver("John Doe", "1234567890");
+		Address address = new Address("123 Main St", "City", "12345");
+		ShippingInfo shippingInfo = new ShippingInfo(receiver,address);
 
 		order = new Order(id, orderer, orderLines, shippingInfo, OrderState.PAYMENT_WAITING);
 
 		orderRepository = mock(OrderRepository.class);
 		given(orderRepository.findById(id))
 			.willReturn(order);
+
 		memberRepository = mock(MemberRepository.class);
-	}
+		Member member = new Member(memberId, address);
+		given(memberRepository.findById(memberId))
+			.willReturn(member);
 
-	@Test
-	void canCreated(){
-		ChangeOrderService service = new ChangeOrderService(orderRepository, memberRepository);
-
-		assertNotNull(service);
+		service = new ChangeOrderService(orderRepository, memberRepository);
 	}
 
 	@Test
 	void shouldChangeShippingInfo_whenOrderExists() {
-		ChangeOrderService service = new ChangeOrderService(orderRepository, memberRepository);
 		ShippingInfo newShippingInfo = createNewShippingInfo();
 
-		Assertions.assertDoesNotThrow(()->service.changeShippingInfo(id, newShippingInfo, false));
+		Assertions.assertDoesNotThrow(()-> service.changeShippingInfo(id, newShippingInfo, false));
+	}
+
+	@Test
+	void shouldChangeAddress_whenUseNewShippingAddrAsMemberAddrIsTrue() {
+		ShippingInfo newShippingInfo = createNewShippingInfo();
+		boolean useNewShippingAddrAsMemberAddr = true;
+
+		Assertions.assertDoesNotThrow(() -> service.changeShippingInfo(id, newShippingInfo, useNewShippingAddrAsMemberAddr));
 	}
 }
