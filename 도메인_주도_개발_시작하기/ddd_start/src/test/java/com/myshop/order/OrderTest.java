5961b3440efe62bb18a68127ae7f2cb4d9dc35d5
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 12:02:31 +0900

    add exception test

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/OrderTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/OrderTest.java
@@ -4,12 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderTest {
 
@@ -20,9 +18,17 @@ class OrderTest {
 		);
 	}
 
+	public static Stream<Arguments> shippingNotChangeableOrderStateSource() {
+		return Stream.of(
+			Arguments.of(OrderState.SHIPPED),
+			Arguments.of(OrderState.DELIVERING),
+			Arguments.of(OrderState.DELIVERY_COMPLETED)
+		);
+	}
+
 	@ParameterizedTest
 	@MethodSource(value = "shippingChangeableOrderStateSource")
-	void changeShippingInfo_shouldChangeShippingInfo_whenOrderStateIsPaymentWaiting(OrderState state) {
+	void shouldChangeShippingInfo_whenOrderStateIsPaymentWaiting(OrderState state) {
 		ShippingInfo oldShippingInfo = new ShippingInfo();
 		Order order = new Order(state, oldShippingInfo);
 		ShippingInfo newShippingInfo = new ShippingInfo();
@@ -30,5 +36,19 @@ class OrderTest {
 		order.changeShippingInfo(newShippingInfo);
 
 		assertDoesNotThrow(() -> order.changeShippingInfo(newShippingInfo));
+	}
+
+	@ParameterizedTest
+	@MethodSource(value = "shippingNotChangeableOrderStateSource")
+	void shouldThrowException_whenOrderStateIsNotShippingChangeable(OrderState state) {
+		ShippingInfo oldShippingInfo = new ShippingInfo();
+		Order order = new Order(state, oldShippingInfo);
+		ShippingInfo newShippingInfo = new ShippingInfo();
+
+		Throwable throwable = Assertions.catchThrowable(() -> order.changeShippingInfo(newShippingInfo));
+
+		Assertions.assertThat(throwable)
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining("can't change shipping in " + state);
 	}
 }
