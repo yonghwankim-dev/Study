722f567aa6f21cccaf09e5efeca54337943f0af6
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 13:29:46 +0900

    add test - Receiver equals and hashCode

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/ReceiverTest.java
@@ -0,0 +1,28 @@
+package com.myshop.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReceiverTest {
+
+	@Test
+	void canCreateReceiver() {
+		String name = "John Doe";
+		String phoneNumber = "123-456-7890";
+
+		Receiver receiver = new Receiver(name, phoneNumber);
+
+		assertNotNull(receiver);
+		assertEquals(name, receiver.getName());
+		assertEquals(phoneNumber, receiver.getPhoneNumber());
+	}
+
+	@Test
+	void shouldBeEqual_whenReceiversHaveSameNameAndPhoneNumber() {
+		Receiver receiver1 = new Receiver("John Doe", "123-456-7890");
+		Receiver receiver2 = new Receiver("John Doe", "123-456-7890");
+
+		assertEquals(receiver1, receiver2);
+	}
+}
