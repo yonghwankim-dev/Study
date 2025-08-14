cac191e8607ee70a0da66c79f61d892733a43adf
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 11:48:19 +0900

    initial commit - ddd_start2

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/DddStartApplication.java
@@ -0,0 +1,13 @@
+package com.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStartApplication.class, args);
+	}
+
+}
