425927f48976ea72eb110d0b42856158e89e7624
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 15:25:19 +0900

    add exception test - orderIsNull

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/NoOrderException.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/NoOrderException.java
@@ -3,7 +3,7 @@ package com.myshop.order;
 import com.myshop.order.domain.OrderNo;
 
 public class NoOrderException extends RuntimeException {
-	public NoOrderException(OrderNo number) {
-		super("Order not found: " + number);
+	public NoOrderException(OrderNo id) {
+		super("Order not found: " + id);
 	}
 }
