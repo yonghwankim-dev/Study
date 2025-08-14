d701bf967044dd2d85ae60bae561e4e3369248ff
yonghwankim-dev <dragonbead95@naver.com>
2025-08-14 15:19:08 +0900

    imple changeOrderService

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/domain/Member.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/domain/Member.java
@@ -4,7 +4,14 @@ import com.myshop.order.domain.Address;
 
 public class Member {
 
+
+	private MemberId memberId;
 	private Address address;
+
+	public Member(MemberId memberId, Address address) {
+		this.memberId = memberId;
+		this.address = address;
+	}
 
 	public void changeAddress(Address address) {
 		if (address == null) {
