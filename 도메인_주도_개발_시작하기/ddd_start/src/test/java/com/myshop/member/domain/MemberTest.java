70babacba9486a6f1739c9a8f53252bdf1fc63c1
yonghwankim-dev <dragonbead95@naver.com>
2025-08-15 14:44:50 +0900

    add exception test - changePassword

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/domain/MemberTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/domain/MemberTest.java
@@ -1,16 +1,20 @@
 package com.myshop.member.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.myshop.order.domain.Address;
 
 class MemberTest {
 
-	@Test
-	void shouldNotThrow_whenCurrentPasswordMatch(){
+	private Member member;
+
+	@BeforeEach
+	void setUp() {
 		MemberId memberId = new MemberId("12345");
 		Address address = new Address(
 			"서울특별시 강남구 역삼동",
@@ -19,9 +23,25 @@ class MemberTest {
 		);
 		String currentPassword = "password1234";
 		Password password = new Password(currentPassword);
-		Member member = new Member(memberId, address, password);
+		member = new Member(memberId, address, password);
+	}
+
+	@Test
+	void shouldNotThrow_whenCurrentPasswordMatch(){
+		String currentPassword = "password1234";
 		String newPassword = "newPassword1234";
 
-		Assertions.assertDoesNotThrow(()-> member.changePassword(currentPassword, newPassword));
+		assertDoesNotThrow(()-> member.changePassword(currentPassword, newPassword));
+	}
+
+	@Test
+	void shouldThrow_whenCurrentPasswordNotMatch() {
+		String notMatchingCurrentPassword = "wrongPassword";
+		String newPassword = "newPassword1234";
+
+		Throwable throwable = catchThrowable(() -> member.changePassword(notMatchingCurrentPassword, newPassword));
+
+		assertThat(throwable)
+			.isInstanceOf(PasswordNotMatchException.class);
 	}
 }
