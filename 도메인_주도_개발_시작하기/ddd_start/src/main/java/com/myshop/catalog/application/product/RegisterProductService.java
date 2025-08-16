656bc39a0ed885666afb7f00d06f18ed4490c98f
yonghwankim-dev <dragonbead95@naver.com>
2025-08-16 14:08:59 +0900

    feat registerNewProduct

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/catalog/application/product/RegisterProductService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/catalog/application/product/RegisterProductService.java
@@ -1,9 +1,26 @@
 package com.myshop.catalog.application.product;
 
+import java.util.Collections;
+import java.util.Set;
+
+import com.myshop.catalog.domain.category.CategoryId;
+import com.myshop.catalog.domain.product.Product;
 import com.myshop.catalog.domain.product.ProductId;
+import com.myshop.catalog.domain.product.ProductRepository;
 
 public class RegisterProductService {
-	public ProductId registerNewProduct(NewProductRequest request){
-		return null;
+
+	private final ProductRepository productRepository;
+
+	public RegisterProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public ProductId registerNewProduct(NewProductRequest request) {
+		ProductId id = new ProductId("9000000112298");
+		Set<CategoryId> categoryIds = Collections.emptySet();
+		Product product = new Product(id, categoryIds);
+		productRepository.save(product);
+		return id;
 	}
 }
--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/catalog/application/product/RegisterProductServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/catalog/application/product/RegisterProductServiceTest.java
@@ -1,16 +1,48 @@
 package com.myshop.catalog.application.product;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.myshop.catalog.domain.product.ProductId;
+import com.myshop.catalog.domain.product.ProductRepository;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RegisterProductServiceTest {
 
+	@Autowired
+	private ProductRepository productRepository;
+	private RegisterProductService service;
+
+	@BeforeEach
+	void setUp() {
+		service = new RegisterProductService(productRepository);
+	}
+
+	@AfterEach
+	void tearDown() {
+		productRepository.deleteAll();
+	}
+
 	@Test
-	void canCreated(){
-		RegisterProductService service = new RegisterProductService();
+	void canCreated() {
 		Assertions.assertThat(service).isNotNull();
+	}
+
+	@Test
+	void shouldSaveNewProduct() {
+		NewProductRequest request = new NewProductRequest();
+
+		ProductId productId = service.registerNewProduct(request);
+
+		Assertions.assertThat(productId).isNotNull();
+		Assertions.assertThat(productId).isEqualTo(new ProductId("9000000112298"));
+		Assertions.assertThat(productRepository.findById(productId)).isNotNull();
 	}
 
 }
