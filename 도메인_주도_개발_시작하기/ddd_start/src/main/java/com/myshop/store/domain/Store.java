9376af59192a3274dbbec0295793f3e0b090b80b
yonghwankim-dev <dragonbead95@naver.com>
2025-08-16 15:29:02 +0900

    create method - createProduct

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/store/domain/Store.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/store/domain/Store.java
@@ -1,5 +1,12 @@
 package com.myshop.store.domain;
 
+import java.util.Collections;
+import java.util.Set;
+
+import com.myshop.catalog.domain.category.CategoryId;
+import com.myshop.catalog.domain.product.Product;
+import com.myshop.catalog.domain.product.ProductId;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -19,5 +26,17 @@ public class Store {
 
 	public StoreId getStoreId() {
 		return storeId;
+	}
+
+	public Product createProduct(ProductId newProductId) {
+		if (isBlocked()) {
+			throw new IllegalStateException("Store is blocked, cannot create new product");
+		}
+		Set<CategoryId> categoryIds = Collections.emptySet();
+		return new Product(newProductId, categoryIds, storeId);
+	}
+
+	private boolean isBlocked() {
+		return false;
 	}
 }
