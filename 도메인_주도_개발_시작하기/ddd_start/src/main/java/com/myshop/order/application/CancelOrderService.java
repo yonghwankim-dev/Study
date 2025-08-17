8139db8aed4ca3dfe4ef4275f4c5602485e80a5f
yonghwankim-dev <dragonbead95@naver.com>
2025-08-17 14:10:57 +0900

    create CancelPolicy, Canceller

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/application/CancelOrderService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/application/CancelOrderService.java
@@ -3,6 +3,8 @@ package com.myshop.order.application;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.myshop.order.OrderNotFoundException;
+import com.myshop.order.domain.CancelPolicy;
+import com.myshop.order.domain.Canceller;
 import com.myshop.order.domain.Order;
 import com.myshop.order.domain.OrderNo;
 import com.myshop.order.domain.OrderRepository;
@@ -16,9 +18,13 @@ public class CancelOrderService {
 	}
 
 	@Transactional
-	public void cancelOrder(OrderNo orderNo) {
+	public void cancelOrder(OrderNo orderNo, Canceller canceller) {
+		CancelPolicy cancelPolicy = new CancelPolicy();
 		Order order = orderRepository.findById(orderNo)
 			.orElseThrow(() -> new OrderNotFoundException(orderNo));
+		if (!cancelPolicy.hasCancellationPermission(order, canceller)) {
+			throw new NoCancellablePermission();
+		}
 		order.cancel();
 	}
 }
