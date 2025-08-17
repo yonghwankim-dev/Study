02933ee964a7b6232254a27ebc107a57423ee0c4
yonghwankim-dev <dragonbead95@naver.com>
2025-08-17 15:22:41 +0900

    add Article domain

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/board/domain/ArticleContent.java
@@ -0,0 +1,25 @@
+package com.myshop.board.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class ArticleContent {
+	@Column(name = "content")
+	private String content;
+	@Column(name = "content_type")
+	private String contentType;
+
+	public ArticleContent(String content, String contentType) {
+		this.content = content;
+		this.contentType = contentType;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+}
