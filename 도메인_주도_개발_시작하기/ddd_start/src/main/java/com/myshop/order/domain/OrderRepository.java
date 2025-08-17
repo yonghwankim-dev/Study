de398cea7f29258822d1a0396e930963e2beb92e
yonghwankim-dev <dragonbead95@naver.com>
2025-08-17 12:57:03 +0900

    add test - findByOrdererId

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/domain/OrderRepository.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/domain/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.myshop.order.domain;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository {
 	Optional<Order> findById(OrderNo id);
+
+	List<Order> findByOrdererId(String ordererId, int startRow, int size);
 
 	void save(Order order);
 
--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/infrastructure/SpringDataJpaOrderRepository.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/infrastructure/SpringDataJpaOrderRepository.java
@@ -1,10 +1,20 @@
 package com.myshop.order.infrastructure;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.myshop.order.domain.Order;
 import com.myshop.order.domain.OrderNo;
 
 public interface SpringDataJpaOrderRepository extends JpaRepository<Order, OrderNo> {
+
+	@Query("SELECT o FROM Order o "
+		+ "WHERE o.orderer.memberId.id = :ordererId "
+		+ "ORDER BY o.orderNo.id DESC")
+	List<Order> findByOrdererId(@Param("ordererId") String ordererId, Pageable pageable);
 
 }
