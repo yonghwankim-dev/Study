698039d9b54734839b10a6341e7c2628ea6c2942
yonghwankim-dev <dragonbead95@naver.com>
2025-08-22 14:49:16 +0900

    add OrderSummary_ static  model

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/query/dao/OrdererIdSpec.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/query/dao/OrdererIdSpec.java
@@ -1,8 +1,9 @@
-package com.myshop.order.infrastructure;
+package com.myshop.order.query.dao;
 
 import org.springframework.data.jpa.domain.Specification;
 
 import com.myshop.order.query.dto.OrderSummary;
+import com.myshop.order.query.dto.OrderSummary_;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -11,8 +12,14 @@ import jakarta.persistence.criteria.Root;
 
 public class OrdererIdSpec implements Specification<OrderSummary> {
 
+	private final String ordererId;
+
+	public OrdererIdSpec(String ordererId) {
+		this.ordererId = ordererId;
+	}
+
 	@Override
-	public Predicate toPredicate(Root<OrderSummary> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-		return null;
+	public Predicate toPredicate(Root<OrderSummary> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		return cb.equal(root.get(OrderSummary_.ordererId), ordererId);
 	}
 }
