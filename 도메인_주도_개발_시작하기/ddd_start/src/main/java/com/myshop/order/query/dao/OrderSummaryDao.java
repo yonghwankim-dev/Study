887dc111ae652b843e513d6d19071e45fa9462bd
yonghwankim-dev <dragonbead95@naver.com>
2025-08-22 16:13:01 +0900

    add test

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/query/dao/OrderSummaryDao.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/query/dao/OrderSummaryDao.java
@@ -8,5 +8,9 @@ import org.springframework.data.repository.Repository;
 import com.myshop.order.query.dto.OrderSummary;
 
 public interface OrderSummaryDao extends Repository<OrderSummary, String> {
+	void save(OrderSummary orderSummary);
+
 	List<OrderSummary> findAll(Specification<OrderSummary> spec);
+
+	List<OrderSummary> findByOrdererIdOrderByNumberDesc(String ordererId);
 }
