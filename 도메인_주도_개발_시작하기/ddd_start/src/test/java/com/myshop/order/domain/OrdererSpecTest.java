6186d5c16ac60da0e9232d7c9986e5303652b854
yonghwankim-dev <dragonbead95@naver.com>
2025-08-22 13:06:13 +0900

    add test - isSatisfiedBy

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/domain/OrdererSpecTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/domain/OrdererSpecTest.java
@@ -2,7 +2,10 @@ package com.myshop.order.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import com.myshop.FixedDomainFactory;
 
 class OrdererSpecTest {
 
@@ -14,4 +17,14 @@ class OrdererSpecTest {
 		assertNotNull(ordererSpec);
 	}
 
+	@Test
+	void shouldReturnTrue_whenOrdererIdIsMatched() {
+		String ordererId = "12345";
+		OrdererSpec ordererSpec = new OrdererSpec(ordererId);
+		Order order = FixedDomainFactory.createOrder();
+
+		boolean satisfiedBy = ordererSpec.isSatisfiedBy(order);
+
+		Assertions.assertThat(satisfiedBy).isTrue();
+	}
 }
