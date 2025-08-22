39f903bc6484638609faea15db779c5f319781d6
yonghwankim-dev <dragonbead95@naver.com>
2025-08-22 13:06:57 +0900

    introduce field - OrderSpec

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/domain/OrdererSpecTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/order/domain/OrdererSpecTest.java
@@ -3,24 +3,29 @@ package com.myshop.order.domain;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.myshop.FixedDomainFactory;
 
 class OrdererSpecTest {
 
+	private String ordererId;
+	private OrdererSpec ordererSpec;
+
+	@BeforeEach
+	void setUp() {
+		ordererId = "12345";
+		ordererSpec = new OrdererSpec(ordererId);
+	}
+
 	@Test
 	void canCreated() {
-		String ordererId = "12345";
-		OrdererSpec ordererSpec = new OrdererSpec(ordererId);
-
 		assertNotNull(ordererSpec);
 	}
 
 	@Test
 	void shouldReturnTrue_whenOrdererIdIsMatched() {
-		String ordererId = "12345";
-		OrdererSpec ordererSpec = new OrdererSpec(ordererId);
 		Order order = FixedDomainFactory.createOrder();
 
 		boolean satisfiedBy = ordererSpec.isSatisfiedBy(order);
