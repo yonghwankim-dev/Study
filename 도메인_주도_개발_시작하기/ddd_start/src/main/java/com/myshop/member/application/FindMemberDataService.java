a3bff404aac1dac83f08eef9f7621c6540ed8c2c
yonghwankim-dev <dragonbead95@naver.com>
2025-08-23 16:43:01 +0900

    create SpecBuilder class

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/application/FindMemberDataService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/application/FindMemberDataService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
+import com.myshop.common.jpa.SpecBuilder;
 import com.myshop.member.query.dao.MemberDataDao;
 import com.myshop.member.query.dao.MemberDataSpecs;
 import com.myshop.member.query.dto.MemberData;
@@ -22,14 +22,10 @@ public class FindMemberDataService {
 	}
 
 	public List<MemberData> findMembers(MemberSearchRequest searchRequest) {
-		Specification<MemberData> spec = Specification.where(null);
-		if (searchRequest.isOnlyNotBlocked()) {
-			spec = spec.and(MemberDataSpecs.nonBlocked());
-		}
-		if (StringUtils.hasText(searchRequest.getName())) {
-			spec = spec.and(MemberDataSpecs.nameLike(searchRequest.getName()));
-		}
-
+		Specification<MemberData> spec = SpecBuilder.builder(MemberData.class)
+			.ifTrue(searchRequest.isOnlyNotBlocked(), MemberDataSpecs::nonBlocked)
+			.ifHasText(searchRequest.getName(), MemberDataSpecs::nameLike)
+			.toSpec();
 		return memberDataDao.findAll(spec, PageRequest.of(0, 5)).getContent();
 	}
 }
