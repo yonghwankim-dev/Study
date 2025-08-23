3bd50a052552bd805ad771fe14105598038b77cd
yonghwankim-dev <dragonbead95@naver.com>
2025-08-23 15:09:39 +0900

    add test - findByNameLike

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/query/dao/MemberDataDaoTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/query/dao/MemberDataDaoTest.java
@@ -1,11 +1,15 @@
 package com.myshop.member.query.dao;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.myshop.member.query.dto.MemberData;
 
@@ -22,6 +26,14 @@ class MemberDataDaoTest {
 		return new MemberData(id, name, blocked);
 	}
 
+	private void createMemberDatas() {
+		for (int i = 1; i <= 20; i++) {
+			String id = String.format("%05d", i);
+			MemberData memberData = createMemberData(id);
+			memberDataDao.save(memberData);
+		}
+	}
+
 	@AfterEach
 	void tearDown() {
 		memberDataDao.deleteAll();
@@ -36,5 +48,16 @@ class MemberDataDaoTest {
 
 		MemberData findMemberData = memberDataDao.findById(id);
 		Assertions.assertThat(findMemberData).isEqualTo(memberData);
+	}
+
+	@Test
+	void shouldReturnMemberDataListByNameAndPageable() {
+		createMemberDatas();
+		String name = "james";
+		Pageable pageable = PageRequest.of(0, 5);
+
+		List<MemberData> memberDataList = memberDataDao.findByNameLike(name, pageable);
+
+		Assertions.assertThat(memberDataList).hasSize(5);
 	}
 }
