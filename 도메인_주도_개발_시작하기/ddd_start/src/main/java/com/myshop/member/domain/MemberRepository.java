e8bcdcda33a022da6fe60cfa4d6b45b049919aef
yonghwankim-dev <dragonbead95@naver.com>
2025-08-24 15:21:05 +0900

    create BlockMemberService

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/domain/MemberRepository.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/domain/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.myshop.member.domain;
 
+import java.util.List;
+
 public interface MemberRepository {
 	Member findById(MemberId id);
+
+	List<Member> findByIdIn(String[] blockingIds);
+
 	void save(Member member);
+
 	void delete(Member member);
 
 	void deleteAll();
