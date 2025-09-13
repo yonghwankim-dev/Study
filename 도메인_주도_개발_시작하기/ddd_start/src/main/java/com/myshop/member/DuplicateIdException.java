15002bbcb29e224b53c0360212666602bf947b35
yonghwankim-dev <dragonbead95@naver.com>
2025-09-13 14:44:35 +0900

    add test - id duplicate case

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/DuplicateIdException.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/DuplicateIdException.java
@@ -1,7 +1,19 @@
 package com.myshop.member;
 
 public class DuplicateIdException extends RuntimeException {
+
+	private final String id;
+
 	public DuplicateIdException(String id) {
 		super("Duplicate id: " + id);
+		this.id = id;
+	}
+
+	public String getPropertyName() {
+		return "id";
+	}
+
+	public String getId() {
+		return id;
 	}
 }
