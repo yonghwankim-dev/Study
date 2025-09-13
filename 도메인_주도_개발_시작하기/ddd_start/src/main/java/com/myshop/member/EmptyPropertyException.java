45b8be9b7bfa082d03079fb9fcf4d1c68d4bffbb
yonghwankim-dev <dragonbead95@naver.com>
2025-09-13 14:06:09 +0900

    add Error Response

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/EmptyPropertyException.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/EmptyPropertyException.java
@@ -1,7 +1,14 @@
 package com.myshop.member;
 
 public class EmptyPropertyException extends RuntimeException {
+	private final String propertyName;
+
 	public EmptyPropertyException(String propertyName) {
 		super(propertyName + " is empty");
+		this.propertyName = propertyName;
+	}
+
+	public String getPropertyName() {
+		return propertyName;
 	}
 }
--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/ui/MemberJoinController.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/ui/MemberJoinController.java
@@ -1,12 +1,18 @@
 package com.myshop.member.ui;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myshop.member.EmptyPropertyException;
 import com.myshop.member.application.JoinService;
 import com.myshop.member.domain.MemberId;
+import com.myshop.member.query.dto.JoinErrorResponse;
 import com.myshop.member.query.dto.JoinRequest;
 
 @RestController
@@ -19,8 +25,18 @@ public class MemberJoinController {
 	}
 
 	@PostMapping("/member/join")
-	public ResponseEntity<MemberId> join(@RequestBody JoinRequest joinRequest) {
-		MemberId memberId = joinService.join(joinRequest);
+	public ResponseEntity<?> join(@RequestBody JoinRequest joinRequest, Errors errors) {
+		MemberId memberId;
+		try {
+			memberId = joinService.join(joinRequest);
+		} catch (EmptyPropertyException e) {
+			errors.rejectValue(e.getPropertyName(), "empty");
+			List<ErrorResponse> errorResponses = errors.getFieldErrors().stream()
+				.map(err -> new JoinErrorResponse(err.getField(), err.getDefaultMessage(), err.getCode()))
+				.map(ErrorResponse.class::cast)
+				.toList();
+			return ResponseEntity.badRequest().body(errorResponses);
+		}
 		return ResponseEntity.ok(memberId);
 	}
 }
