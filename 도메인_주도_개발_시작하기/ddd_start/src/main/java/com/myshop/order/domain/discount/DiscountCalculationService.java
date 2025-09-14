e1021fba16e3ac05dbb8174b7d06ddf81733547d
yonghwankim-dev <dragonbead95@naver.com>
2025-09-14 16:14:58 +0900

    create method - calculateDiscountAmounts

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/domain/discount/DiscountCalculationService.java
@@ -0,0 +1,10 @@
+package com.myshop.order.domain.discount;
+
+import com.myshop.common.model.Money;
+
+public class DiscountCalculationService {
+
+	public Money calculateDiscountAmounts() {
+		return null;
+	}
+}
