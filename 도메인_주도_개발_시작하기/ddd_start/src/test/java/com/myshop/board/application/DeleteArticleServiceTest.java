f533a42205cbd882dc626ff6c6f77e7692d20c40
yonghwankim-dev <dragonbead95@naver.com>
2025-09-14 14:08:58 +0900

    add method - markDeleted

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/board/application/DeleteArticleServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/board/application/DeleteArticleServiceTest.java
@@ -1,15 +1,48 @@
 package com.myshop.board.application;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.myshop.board.domain.Article;
+import com.myshop.board.domain.ArticleContent;
+import com.myshop.board.domain.ArticleRepository;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DeleteArticleServiceTest {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@Autowired
+	private DeleteArticleService service;
+	private Long articleId;
+
+	@BeforeEach
+	void setUp() {
+		ArticleContent articleContent = new ArticleContent("content", "text/plain");
+		Article article = new Article("Title 1", articleContent);
+		articleRepository.save(article);
+		articleId = article.getId();
+	}
 
 	@Test
 	void canCreated() {
-		DeleteArticleService service = new DeleteArticleService();
-
 		Assertions.assertThat(service).isNotNull();
+	}
+
+	@Test
+	void delete() {
+		String userId = "member-1";
+
+		service.delete(userId, articleId);
+
+		Article findArticle = articleRepository.findById(articleId);
+		Assertions.assertThat(findArticle.isDeleted()).isTrue();
 	}
 
 }
