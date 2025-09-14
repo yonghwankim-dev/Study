1815c89adeaa997fb4fe63ef802dcfc032c11f39
yonghwankim-dev <dragonbead95@naver.com>
2025-09-14 13:24:32 +0900

    add PreAuthorize annotiation

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/application/BlockMembersService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/member/application/BlockMembersService.java
@@ -2,10 +2,12 @@ package com.myshop.member.application;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.myshop.member.domain.Member;
+import com.myshop.member.domain.MemberId;
 import com.myshop.member.domain.MemberRepository;
 
 @Service
@@ -15,6 +17,13 @@ public class BlockMembersService {
 
 	public BlockMembersService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
+	}
+
+	@Transactional
+	@PreAuthorize("hasRole('ADMIN')")
+	public void block(String memberId) {
+		Member member = memberRepository.findById(new MemberId(memberId));
+		member.block();
 	}
 
 	@Transactional
--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/application/BlockMembersServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/application/BlockMembersServiceTest.java
@@ -1,10 +1,12 @@
 package com.myshop.member.application;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 
 import com.myshop.FixedDomainFactory;
 import com.myshop.member.domain.Member;
@@ -37,5 +39,14 @@ class BlockMembersServiceTest {
 		memberRepository.findByIdIn(memberIds).forEach(member -> {
 			assert member.isBlocked() : "Member should be blocked";
 		});
+	}
+
+	@Test
+	void shouldNotBlockMember_whenNoAuthentication() {
+		String memberId = memberIds[0];
+		Throwable throwable = Assertions.catchThrowable(() -> service.block(memberId));
+
+		Assertions.assertThat(throwable)
+			.isInstanceOf(AuthenticationCredentialsNotFoundException.class);
 	}
 }
