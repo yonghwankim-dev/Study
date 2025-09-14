bb8e20ab80d72fdfb8edd5b56cacab9f5fa99c11
yonghwankim-dev <dragonbead95@naver.com>
2025-09-14 13:34:52 +0900

    update test - add SecurityContext

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/application/BlockMembersServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/application/BlockMembersServiceTest.java
@@ -1,12 +1,19 @@
 package com.myshop.member.application;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.myshop.FixedDomainFactory;
 import com.myshop.member.domain.Member;
@@ -30,6 +37,21 @@ class BlockMembersServiceTest {
 			Member member = FixedDomainFactory.createMember(memberId);
 			memberRepository.save(member);
 		}
+
+		// SecurityContext에 ADMIN 권한을 가진 사용자 등록
+		UsernamePasswordAuthenticationToken authentication =
+			new UsernamePasswordAuthenticationToken(
+				"admin",           // principal
+				"password",        // credentials
+				List.of(new SimpleGrantedAuthority("ROLE_ADMIN")) // 권한
+			);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+	@AfterEach
+	void tearDown() {
+		memberRepository.deleteAll();
+		SecurityContextHolder.clearContext();
 	}
 
 	@Test
@@ -43,10 +65,31 @@ class BlockMembersServiceTest {
 
 	@Test
 	void shouldNotBlockMember_whenNoAuthentication() {
+		// 미인증 상태로 초기화
+		SecurityContextHolder.clearContext();
+
 		String memberId = memberIds[0];
 		Throwable throwable = Assertions.catchThrowable(() -> service.block(memberId));
 
 		Assertions.assertThat(throwable)
 			.isInstanceOf(AuthenticationCredentialsNotFoundException.class);
+	}
+
+	@Test
+	void shouldNotBlockMember_whenNoPermission() {
+		// SecurityContext에 USER 권한만 가진 사용자 등록
+		UsernamePasswordAuthenticationToken authentication =
+			new UsernamePasswordAuthenticationToken(
+				"user",            // principal
+				"password",        // credentials
+				List.of(new SimpleGrantedAuthority("ROLE_USER")) // 권한
+			);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String memberId = memberIds[0];
+		Throwable throwable = Assertions.catchThrowable(() -> service.block(memberId));
+
+		Assertions.assertThat(throwable)
+			.isInstanceOf(AccessDeniedException.class);
 	}
 }
