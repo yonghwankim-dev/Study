d57f2abad75a6954446df4e976a88fda66b9fca1
yonghwankim-dev <dragonbead95@naver.com>
2025-09-16 15:57:39 +0900

    add test - Coupon.applyDiscount

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/coupon/domain/CouponTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/coupon/domain/CouponTest.java
@@ -3,6 +3,9 @@ package com.myshop.coupon.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.myshop.FixedDomainFactory;
+import com.myshop.common.model.Money;
+
 class CouponTest {
 
 	@Test
@@ -12,4 +15,12 @@ class CouponTest {
 		Assertions.assertThat(coupon).isNotNull();
 	}
 
+	@Test
+	void applyDiscount() {
+		Coupon coupon = new Coupon(0.1);
+
+		Money money = coupon.applyDiscount(FixedDomainFactory.createOrderLine("product-1", 1000, 2));
+
+		Assertions.assertThat(money).isEqualTo(new Money(200));
+	}
 }
