52c65c4c2ce94ea0013822779d63dde1b51d621c
yonghwankim-dev <dragonbead95@naver.com>
2025-09-21 13:05:12 +0900

    fix field name - requesterId

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/survey/query/dto/CreateSurveyRequest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/survey/query/dto/CreateSurveyRequest.java
@@ -2,12 +2,18 @@ package com.myshop.survey.query.dto;
 
 public class CreateSurveyRequest {
 	private String title;
+	private Long requesterId;
 
-	public CreateSurveyRequest(String title) {
+	public CreateSurveyRequest(String title, Long requesterId) {
 		this.title = title;
+		this.requesterId = requesterId;
 	}
 
 	public String getTitle() {
 		return title;
+	}
+
+	public Long getRequesterId() {
+		return requesterId;
 	}
 }
