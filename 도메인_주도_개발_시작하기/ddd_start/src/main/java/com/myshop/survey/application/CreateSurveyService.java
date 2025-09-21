01e8770989c05aeb49bbc980672346ad45c9d89a
yonghwankim-dev <dragonbead95@naver.com>
2025-09-21 12:43:05 +0900

    create method - createSurvey

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/survey/application/CreateSurveyService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/survey/application/CreateSurveyService.java
@@ -2,6 +2,11 @@ package com.myshop.survey.application;
 
 import org.springframework.stereotype.Service;
 
+import com.myshop.survey.query.dto.CreateSurveyRequest;
+
 @Service
 public class CreateSurveyService {
+	public Long createSurvey(CreateSurveyRequest request) {
+		return 1L;
+	}
 }
