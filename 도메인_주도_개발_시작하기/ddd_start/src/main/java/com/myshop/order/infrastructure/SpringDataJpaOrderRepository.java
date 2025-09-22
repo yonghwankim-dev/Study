7a43eba56e80c61e3652f8cf8dfcad4e9c499c76
yonghwankim-dev <dragonbead95@naver.com>
2025-09-22 14:59:12 +0900

    add @QueryHints

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/infrastructure/SpringDataJpaOrderRepository.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/infrastructure/SpringDataJpaOrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import com.myshop.order.domain.model.Order;
@@ -14,6 +15,7 @@ import com.myshop.order.domain.model.OrderNo;
 import com.myshop.order.query.dto.OrderSummary;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 
 public interface SpringDataJpaOrderRepository extends JpaRepository<Order, OrderNo> {
 
@@ -27,6 +29,7 @@ public interface SpringDataJpaOrderRepository extends JpaRepository<Order, Order
 		@Param("size") int size);
 
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@QueryHints(@QueryHint(name = "javax.persistence.lock.timeout", value = "2000"))
 	@Query("SELECT o FROM Order o WHERE o.orderNo = :id")
 	Optional<Order> findByIdForUpdate(@Param("id") OrderNo id);
 
