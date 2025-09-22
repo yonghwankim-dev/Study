9b68b26dd3d210fed702dd8c17863d5ba34838f7
yonghwankim-dev <dragonbead95@naver.com>
2025-09-22 15:46:58 +0900

    add test - LockWaitTimeoutExceeded case

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/infrastructure/JpaMemberRepositoryTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/member/infrastructure/JpaMemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.myshop.FixedDomainFactory;
@@ -80,4 +81,45 @@ class JpaMemberRepositoryTest {
 			.isGreaterThan(thread1EndTime.get());
 	}
 
+	@DisplayName("회원 애그리거트 점유를 대기하다가 타임아웃 발생함")
+	@Test
+	void shouldThrowException_whenLockWaitTimeoutExceeded() throws InterruptedException {
+		CountDownLatch thread1CountDownLatch = new CountDownLatch(1);
+
+		Thread thread1 = new Thread(() -> {
+			transactionTemplate.executeWithoutResult(status -> {
+				Member member = repository.findByIdForUpdate(new MemberId("member-1")).orElseThrow();
+				System.out.println("Thread 1 acquired lock on member: " + member.getId().getId());
+				thread1CountDownLatch.countDown(); // thread1이 락 잡음 알림
+				// 트랜잭션을 오래 유지하여 thread2가 기다리도록 만듦
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			});
+		});
+
+		Thread thread2 = new Thread(() -> {
+			try {
+				thread1CountDownLatch.await(); // thread1이 락 잡을 때까지 대기
+				transactionTemplate.executeWithoutResult(status -> {
+					Throwable throwable = Assertions.catchThrowable(() -> {
+						Member member = repository.findByIdForUpdate(new MemberId("member-1")).orElseThrow();
+						System.out.println("Thread 2 acquired lock on member: " + member.getId().getId());
+					});
+					Assertions.assertThat(throwable)
+						.isInstanceOf(PessimisticLockingFailureException.class);
+				});
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		});
+
+		thread1.start();
+		thread2.start();
+
+		thread1.join();
+		thread2.join();
+	}
 }
