78aac50116f1ebe0b93ce5d5b978e99c5324342a
yonghwankim-dev <dragonbead95@naver.com>
2025-09-23 12:41:20 +0900

    create LockManager

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/lock/domain/LockId.java
@@ -0,0 +1,13 @@
+package com.myshop.lock.domain;
+
+public class LockId {
+	private String value;
+
+	public LockId(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/lock/error/LockException.java
@@ -0,0 +1,7 @@
+package com.myshop.lock.error;
+
+public class LockException extends RuntimeException {
+	public LockException() {
+		super();
+	}
+}
