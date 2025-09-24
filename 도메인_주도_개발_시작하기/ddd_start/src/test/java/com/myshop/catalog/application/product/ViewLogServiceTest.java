1ab7eabd6e0f86b216e8fe2fc189a0e839cbde92
yonghwankim-dev <dragonbead95@naver.com>
2025-09-24 14:19:53 +0900

    introduce field - ViewLogService

--- 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/catalog/application/product/ViewLogServiceTest.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/test/java/com/myshop/catalog/application/product/ViewLogServiceTest.java
@@ -3,20 +3,25 @@ package com.myshop.catalog.application.product;
 import java.time.LocalDateTime;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ViewLogServiceTest {
 
+	private ViewLogService service;
+
+	@BeforeEach
+	void setUp() {
+		service = new ViewLogService();
+	}
+
 	@Test
 	void canCreated() {
-		ViewLogService service = new ViewLogService();
-
 		Assertions.assertThat(service).isNotNull();
 	}
 
 	@Test
 	void appendViewLog() {
-		ViewLogService service = new ViewLogService();
 		String memberId = "member-1";
 		String productId = "product-1";
 		LocalDateTime time = LocalDateTime.now();
