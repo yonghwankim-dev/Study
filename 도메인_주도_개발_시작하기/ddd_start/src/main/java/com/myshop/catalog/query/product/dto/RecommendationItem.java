a1007731ea9a9caf38a197a8c7f0a3ae22dddeb9
yonghwankim-dev <dragonbead95@naver.com>
2025-09-24 14:01:57 +0900

    imple ResSystemClient code'

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/catalog/query/product/dto/RecommendationItem.java
@@ -0,0 +1,14 @@
+package com.myshop.catalog.query.product.dto;
+
+public class RecommendationItem {
+
+	private String itemId;
+
+	public RecommendationItem(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+}
