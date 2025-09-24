72dd6dc45096d28c743774ac7157abf8065e76aa
yonghwankim-dev <dragonbead95@naver.com>
2025-09-24 16:45:18 +0900

    imple event handler

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/domain/model/OrderCanceledEvent.java
@@ -0,0 +1,16 @@
+package com.myshop.order.domain.model;
+
+import com.myshop.common.event.Event;
+
+public class OrderCanceledEvent extends Event {
+	private String orderNumber;
+
+	public OrderCanceledEvent(String orderNumber) {
+		super();
+		this.orderNumber = orderNumber;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+}
