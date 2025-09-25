c249ff2765eda739cca4c13a4de621f712054e61
yonghwankim-dev <dragonbead95@naver.com>
2025-09-25 15:19:27 +0900

    create cancel button

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/application/ChangeOrderStateService.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/application/ChangeOrderStateService.java
@@ -5,16 +5,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.myshop.order.domain.model.Order;
 import com.myshop.order.domain.model.OrderState;
-import com.myshop.order.domain.repository.OrderRepository;
 
 @Service
 public class ChangeOrderStateService {
-
-	private final OrderRepository orderRepository;
-
-	public ChangeOrderStateService(OrderRepository orderRepository) {
-		this.orderRepository = orderRepository;
-	}
 
 	@Transactional
 	public void changeOrderState(Order order, OrderState state) {
--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/ui/CancelOrderController.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/ui/CancelOrderController.java
@@ -2,16 +2,16 @@ package com.myshop.order.ui;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.myshop.member.query.dto.MemberAuthentication;
 import com.myshop.order.application.CancelOrderService;
 import com.myshop.order.domain.model.Canceller;
 import com.myshop.order.domain.model.OrderNo;
 
-@RestController
+@Controller
 public class CancelOrderController {
 
 	private final CancelOrderService service;
@@ -21,11 +21,11 @@ public class CancelOrderController {
 	}
 
 	@PostMapping("/orders/cancel")
-	public String cancelOrder(@RequestParam String orderId) {
+	public String cancelOrder(@RequestParam("id") String orderId) {
 		String memberId = getAuthenticateMemberId();
 		Canceller canceller = new Canceller(memberId);
 		service.cancelOrder(new OrderNo(orderId), canceller);
-		return "Order canceled";
+		return "redirect:/orders";
 	}
 
 	private String getAuthenticateMemberId() {
