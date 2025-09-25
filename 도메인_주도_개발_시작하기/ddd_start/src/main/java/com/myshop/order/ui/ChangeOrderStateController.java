01f9a9a9406415baabaac85c22d34f1856d8bad4
yonghwankim-dev <dragonbead95@naver.com>
2025-09-25 15:33:47 +0900

    Add ChangeOrderState controller

---
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/ui/ChangeOrderStateController.java
@@ -0,0 +1,26 @@
+package com.myshop.order.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.myshop.order.application.ChangeOrderStateService;
+import com.myshop.order.domain.model.OrderNo;
+import com.myshop.order.domain.model.OrderState;
+
+@Controller
+public class ChangeOrderStateController {
+
+	private final ChangeOrderStateService service;
+
+	public ChangeOrderStateController(ChangeOrderStateService service) {
+		this.service = service;
+	}
+
+	@PostMapping("/orders/change-state")
+	public String changeOrderState(@RequestParam("id") String orderId, @RequestParam("state") String state) {
+		OrderState orderState = OrderState.valueOf(state);
+		service.changeOrderState(new OrderNo(orderId), orderState);
+		return "redirect:/orders";
+	}
+}
