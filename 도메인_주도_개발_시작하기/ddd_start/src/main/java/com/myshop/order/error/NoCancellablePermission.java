50576aef2369dd2cca6d47691addbdfde29196b1
yonghwankim-dev <dragonbead95@naver.com>
2025-09-26 13:17:40 +0900

    add no args constructor in NoCancellablePermission class

--- 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/error/NoCancellablePermission.java
+++ 도메인_주도_개발_시작하기/ddd_start/src/main/java/com/myshop/order/error/NoCancellablePermission.java
@@ -1,4 +1,7 @@
 package com.myshop.order.error;
 
 public class NoCancellablePermission extends RuntimeException {
+	public NoCancellablePermission() {
+		super("No permission to cancel order");
+	}
 }
